4557849c329e034fa27d7108f01fc1a816c61c65
Vladislav <Tailingen@mail.ru>
2025-10-19 14:55:35 +0500

    create task get and task create functional

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+db_immitation = [
+    {"id": 1, "text": "Подоить корову"},
+    {"id": 2, "text": "Погулять с собакой"}
+]
+
+@app.get("/home/")
+def home():
+    return "Home"
+
+@app.get("/about/")
+def about():
+    return "Страница о сайте"
+
+@app.get("/tasks/{task_id}/")
+def task(task_id):
+    if int(task_id) <= len(db_immitation):
+        return db_immitation[int(task_id)-1]["text"]
+    else:
+        return "Запись не найдена"
+
+@app.get("/tasks/")
+def tasks():
+    return db_immitation
+
+@app.post("/tasks/create/")
+def task_create(text):
+    db_immitation.append({"id": len(db_immitation)+1, "text": text})
+    return db_immitation[-1]
